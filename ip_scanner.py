46089489c1066ed8b7f9c473fba9de4d0a1ccae4
jrebaza-gt <fashnozzle@gmail.com>
2021-08-10 16:37:29 -0500

    update

--- ip_scanner.py
+++ ip_scanner.py
@@ -1,10 +1,17 @@
 from scapy.all import ARP, Ether, srp
 
 class IPScanner:
-    def __init__(self):
-        self.target_ip_address = input('Enter the target IP Address --> ')
-        self.available_devices = []
+    def __init__(self, ip=None):
+        print(ip)
+        if ip is None:
+            self.target_ip_address = input('Enter the target IP Address --> ')
+        else:
+            self.target_ip_address = ip
+        self._available_devices = []
+        self.scapy_scan()
 
+    def get_available_devices(self):
+        return self._available_devices
 
     def scapy_scan(self):
         # Adding /24 to the end of the target ip in order to search through
@@ -19,13 +26,12 @@ class IPScanner:
 
         for sent, received in result:
             # only storing connected ip addresses
-            self.available_devices.append(received.psrc)
+            self._available_devices.append(received.psrc)
         
         print('Available devices in the network:')
         print('\nIP Address')
-        for ip in self.available_devices:
+        for ip in self._available_devices:
             print(ip)
 
+#network = IPScanner()
 
-network = IPScanner()
-network.scapy_scan()
